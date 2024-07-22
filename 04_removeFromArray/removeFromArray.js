d4c5be182b9417ab1f793a2264c4c6bf88c44a77
getlouied <derienzo_louis@yahoo.com>
2024-07-22 11:22:36 -0400

    Complete javascript exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = array.filter((argument) => {
+        return !args.includes(argument);
+    });
+    return result
 };
 
 // Do not edit below this line
