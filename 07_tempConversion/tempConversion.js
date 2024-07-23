d3258736192d3f8137ee8b3dd669825ac363c263
getlouied <derienzo_louis@yahoo.com>
2024-07-22 20:54:21 -0400

    Complete Javascript exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let tempInCelsius = (5/9)* (temperature - 32);
+
+  return Number(tempInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let tempInFahrenheit = ((9/5) * temperature) + 32;
+
+  return Number(tempInFahrenheit.toFixed(1));
+
 };
 
 // Do not edit below this line
