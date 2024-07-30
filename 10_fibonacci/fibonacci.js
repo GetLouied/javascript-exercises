4c5a6db10ce766cd62ee2b14f200b52997d82c2e
getlouied <derienzo_louis@yahoo.com>
2024-07-30 09:42:07 -0400

    Complete javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let startingNum = index + 1
+    let fibonacciSeq = [1, 1];
+    let totalCounter = 0;
+
+    if (index < 0) {
+        return "OOPS"
+    } else if (index === 0 || index === "0") {
+        return 0
+    }
+
+    for (let i = 1; i < startingNum; i++){
+        totalCounter = fibonacciSeq[i] + fibonacciSeq[i - 1];
+        fibonacciSeq.push(totalCounter)
+        console.log(totalCounter);
+        console.log(fibonacciSeq);
+    }
+
+    return fibonacciSeq[index - 1];
 
 };
 
