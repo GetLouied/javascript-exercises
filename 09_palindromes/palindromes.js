ea330cc1161c29fbccf37c4b237a86cc351c37cf
getlouied <derienzo_louis@yahoo.com>
2024-07-30 09:01:02 -0400

    Complete javascript exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let stringArray = str.toLowerCase().replace(/[^A-Za-z0-9\s]/g, '').split(' ');
+    let palArray = stringArray.join('');
+    let checkArray = palArray.slice().split('').reverse().join('');
+    return palArray === checkArray;
 
 };
 
