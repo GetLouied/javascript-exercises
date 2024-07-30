308d5875c76d748962296e0e5e1842a88b3b7bf9
getlouied <derienzo_louis@yahoo.com>
2024-07-30 11:05:11 -0400

    Complete javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let sortedArray = array.sort(compareAge);
+    console.log(sortedArray);
 
+    return sortedArray[0];
 };
+
+function compareAge(personA, personB) {
+
+    let personAAge;
+    let personBAge;
+
+    if (!personA.yearOfDeath) {
+        personAAge = (new Date().getFullYear() - personA.yearOfBirth)
+    } else {
+        personAAge = (personA.yearOfDeath - personA.yearOfBirth);
+    }
+    
+    if (!personB.yearOfDeath) {
+        personBAge = (new Date().getFullYear() - personB.yearOfBirth);
+    } else {
+        personBAge = (personB.yearOfDeath - personB.yearOfBirth);
+    }
+
+    let comparison = 0;
+    if (personAAge > personBAge) {
+        comparison = -1;
+    } else if (personAAge < personBAge) {
+        comparison = 1;
+    }
+
+    console.log(comparison)
+
+    return comparison;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
